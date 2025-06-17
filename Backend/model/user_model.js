7c5af27159b15a35f304e3afd971c5ac6273cf46
Thyon3 <asnakemengesha80@gmail.com>
2025-06-17 13:06:57 -0400

    passwod hashing

--- Backend/model/user_model.js
+++ Backend/model/user_model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../config/db");
+const bcrpt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -12,6 +13,17 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+userSchema.pre("save", async function () {
+  try {
+    const user = this;
+    const salt = await bcrpt.genSalt(10);
+    const hashPass = await bcrpt.hash(user.password, salt);
+
+    user.password = hashPass;
+  } catch (e) {
+    throw e;
+  }
 });
 
 const UserModel = db.model("user", userSchema);
