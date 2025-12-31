c4007303bbe85a4f61be74c08a81ffd23d36d402
Thyon3 <asnakemengesha80@gmail.com>
2025-12-31 06:57:46 -0500

    feat: add asset paths constants

---
+++ frontend/lib/constants/route_names.dart
@@ -0,0 +1,11 @@
+class RouteNames {
+  static const String home = '/';
+  static const String addTask = '/add-task';
+  static const String editTask = '/edit-task';
+  static const String taskDetail = '/task-detail';
+  static const String categories = '/categories';
+  static const String settings = '/settings';
+  static const String statistics = '/statistics';
+  static const String calendar = '/calendar';
+  static const String about = '/about';
+}
