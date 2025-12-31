6100db00fff36ddfe0d0f921c0dd43766755b865
Thyon3 <asnakemengesha80@gmail.com>
2025-12-31 07:05:53 -0500

    refactor: extract date formatting logic

---
+++ frontend/lib/widgets/gradient_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  final Widget child;
+  final List<Color> colors;
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final BorderRadius? borderRadius;
+
+  const GradientContainer({
+    Key? key,
+    required this.child,
+    required this.colors,
+    this.begin = Alignment.topLeft,
+    this.end = Alignment.bottomRight,
+    this.borderRadius,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: begin,
+          end: end,
+        ),
+        borderRadius: borderRadius,
+      ),
+      child: child,
+    );
+  }
+}
