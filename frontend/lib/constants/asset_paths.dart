dd2cb372ea3892f7f2e7ce0a6f1f84ac0fa2de90
Thyon3 <asnakemengesha80@gmail.com>
2025-12-31 06:57:47 -0500

    refactor: improve error handling consistency

---
+++ frontend/lib/constants/asset_paths.dart
@@ -0,0 +1,16 @@
+class AssetPaths {
+  // Images
+  static const String logo = 'assets/images/logo.png';
+  static const String emptyState = 'assets/images/empty_state.png';
+  static const String onboarding1 = 'assets/images/onboarding_1.png';
+  static const String onboarding2 = 'assets/images/onboarding_2.png';
+  static const String onboarding3 = 'assets/images/onboarding_3.png';
+  
+  // Icons
+  static const String taskIcon = 'assets/icons/task.png';
+  static const String categoryIcon = 'assets/icons/category.png';
+  
+  // Animations
+  static const String loadingAnimation = 'assets/animations/loading.json';
+  static const String successAnimation = 'assets/animations/success.json';
+}
